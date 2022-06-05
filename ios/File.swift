427d653248cf7e28c9020ea4d055c0ac4bd90551
in-ch <sxin2949@naver.com>
2022-06-05 22:36:49 +0900

    feat: 카카오 쉐어 연결

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  foro
+//
+//  Created by 인치 on 2022/06/05.
+//
+
+import Foundation
--- src/c_screens/Login/LoginContainer.tsx
+++ src/c_screens/Login/LoginContainer.tsx
@@ -16,6 +16,52 @@ import LoginPresenter from './LoginPresenter';
 import {hideBottomBar} from '../../apollo/apollo';
 import {RootTabParamList} from '../../navigation/RootNavigation';
 
+import KakaoShareLink from 'react-native-kakao-share-link';
+
+// const kakaoshare = async () => {
+//   try {
+//     const response = await KakaoShareLink.sendCommerce({
+//       content: {
+//         title: 'title',
+//         imageUrl:
+//           'http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg',
+//         link: {
+//           webUrl: 'https://developers.kakao.com/',
+//           mobileWebUrl: 'https://developers.kakao.com/',
+//         },
+//         description: 'description',
+//       },
+//       commerce: {
+//         regularPrice: 100000,
+//         discountPrice: 80000,
+//         discountRate: 20,
+//       },
+//       buttons: [
+//         {
+//           title: '앱에서 보기',
+//           link: {
+//             androidExecutionParams: [{key: 'key1', value: 'value1'}],
+//             iosExecutionParams: [
+//               {key: 'key1', value: 'value1'},
+//               {key: 'key2', value: 'value2'},
+//             ],
+//           },
+//         },
+//         {
+//           title: '웹에서 보기',
+//           link: {
+//             webUrl: 'https://developers.kakao.com/',
+//             mobileWebUrl: 'https://developers.kakao.com/',
+//           },
+//         },
+//       ],
+//     });
+//     console.log(response);
+//   } catch (e) {
+//     console.error(e);
+//     console.error(e.message);
+//   }
+// };
 interface Props {
   navigation: NavigationProp<RootTabParamList, 'Home'>;
 }
